544b9f76a2970a64897a6ee00d5692a95b8c0a55
Alexander <shurik-k2@yandex.ru>
2017-06-17 12:28:31 +0300

    Edit header in p711a2017_Part1.sql in branch newF

--- Calc/p711a2017_Part1.sql
+++ Calc/p711a2017_Part1.sql
@@ -97,7 +97,9 @@ Create table #SECURITIES(
 if exists (select * from sysobjects where id = object_id('dbo.p711a2017_Part1') and OBJECTPROPERTY(id, N'IsProcedure') = 1)
     drop procedure dbo.p711a2017_Part1
 GO
-
+-----------------------------------------
+-- in Git master branch!!!
+-----------------------------------------
 Create Proc dbo.p711a2017_Part1(
       @Date      datetime
 	, @PrintProt tinyint
